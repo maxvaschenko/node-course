bb1a9f45b88b2c42d13f8d9f4a9ce0b96843cac4
Max Vashchenko <maxvaschenko@gmail.com>
2020-01-02 18:39:43 +0200

    +add: forecast logic chaining added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,14 @@
 const { geocode } = require("./utils/geocode");
 const { forecast } = require("./utils/forecast");
 
-geocode("Boston", (err, data) => {
-  console.log("error", err);
-  console.log("data", data);
-});
-
-forecast(37.8267, -122.4233, (err, data) => {
-  console.log("error", err);
-  console.log("data", data);
+geocode("kiev", (err, data) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    const { lat, lon, location } = data;
+    console.log(location);
+    forecast(lat, lon, (err, forecastData) => {
+      console.log("data", forecastData);
+    });
+  }
 });
